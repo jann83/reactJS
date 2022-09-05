e46fa9de09b042e9cab0af247032b5d9558a8c52
jann83 <83481345+jann83@users.noreply.github.com>
2022-09-05 15:40:15 -0500

    05092022

---
+++ FundamentosJS/FundamentosJS/js/03.js
@@ -0,0 +1,10 @@
+// const primerSymbol = Symbol(30)
+// const segundoSymbol = Symbol(30)
+
+// console.log( primerSymbol === segundoSymbol)
+// console.log(primerSymbol.valueOf() )
+
+
+const descuento = null
+console.log(typeof descuento)
+
